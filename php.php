45e8393d47c8cbe0c616e73e1634aa4eda0467e1
Henrique Blem <freirehb@gmail.com>
2025-09-29 07:58:48 -0400

    first commit

---
+++ php.php
@@ -0,0 +1,49 @@
+<?php
+
+echo "Meu nome é Henrique";
+echo "\n";
+
+$nota1 = 8;
+$nota2 = 7;
+$nota3 = 9;
+$media = ($nota1 + $nota2 + $nota3) / 3;
+echo "A média é: " . $media . "\n";
+
+$metros = 2.5;
+$centimetros = $metros * 100;
+
+echo "$metros metros é igual a $centimetros centímetros.\n";
+
+//Para ano bissexto
+
+
+$ano = 2025;
+$timestamp = mktime(0, 0, 0, 1, 1, $ano);
+$isLeapYear = date('L', $timestamp);
+
+if ($isLeapYear) {
+    echo "$ano é um ano bissexto.";
+} else {
+    echo "$ano não é um ano bissexto.";
+}
+
+echo "\n";
+
+//Celsius para Fahrenheit
+
+function celsiusParaFahrenheit($celsius) {
+  $fahrenheit = ($celsius * 9/5) + 32; // Ou ($celsius * 1.8) + 32
+  return $fahrenheit;
+}
+
+// Exemplo de uso:
+$temperaturaCelsius = 25;
+$temperaturaFahrenheit = celsiusParaFahrenheit($temperaturaCelsius);
+
+echo "{$temperaturaCelsius}°C é igual a {$temperaturaFahrenheit}°F";
+
+echo "\n";
+
+echo "Fim da atividade."
+
+?>
---
+++ screem-match.php
@@ -0,0 +1,63 @@
+<?php
+
+echo "Bem-vindo ao Screem Match!\n";
+echo "\n";
+
+$nomeFilme = "Interestelar";
+$anoDeLancamento = 2014;
+
+$quantidadeDeNotas = $argc - 1; // Subtraindo 1 para não contar o nome do script(na posição 0)
+$somadeNotas = 0;
+
+for ($contador = 1; $contador < $argc; $contador++) {
+    $somadeNotas += $argv[$contador];
+}
+
+$notaDoFilme = $somadeNotas / $quantidadeDeNotas;
+
+
+$planoPrime = true;
+$inclusoNoPlano = $planoPrime || $anoDeLancamento < 2020;
+
+echo "Filme: " . $nomeFilme . "\n"; //concatenação
+echo "Ano de Lançamento: $anoDeLancamento\n"; //interpolação
+echo "Nota do Usuário: $notaDoFilme\n";
+
+if ($anoDeLancamento >= 2024) {
+    echo "O filme ainda é novo.\n";
+} elseif ($anoDeLancamento > 2020 && $anoDeLancamento <= 2023) {
+    echo "O filme ainda é recente.\n";
+} else {
+    echo "o filme é antigo.\n";
+}
+
+# Uma alternativa ao if/else é o switch/case, uma estrutura de múltipla escolha que permite testar uma variável contra vários valores possíveis.
+
+switch ($notaDoFilme) {
+    case 10:
+        echo "Avaliação: Excelente!\n";
+        break;
+    case 9:
+        echo "Avaliação: Muito Bom!\n";
+        break;
+    case 8:
+        echo "Avaliação: Bom!\n";
+        break;
+    case 7:
+        echo "Avaliação: Razoável!\n";
+        break;
+    case 6:
+        echo "Avaliação: Regular!\n";
+        break;
+    default:
+        echo "Avaliação: Ruim!\n";
+        break;
+}
+
+//Operadores ternários são uma forma concisa de expressar uma condição if/else em uma única linha.
+
+// $resultado = $expressao1 ? expressao2 : expressao3;
+//Exemplo:
+# $avaliacao = ($mediaNotas >= 9) ? "Excelente" : (($mediaNotas >= 7) ? "Bom" : "Regular");
+
+
